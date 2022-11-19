9616b61f953391f37bbeac326df959e5df4b6f53
citrus-candy <kyuuri11013@gmail.com>
2022-11-20 02:49:19 +0900

    add: Fabricjs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
+  // 画面サイズからvhを100%にする
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+  // Fabricjsの初期設定
+  const canvas = new fabric.Canvas("canvas", {
+    isDrawingMode: true,
+    freeDrawingBrush: new fabric.PencilBrush(canvas)
+  });
+  canvas.freeDrawingBrush.width = 5;
+  canvas.freeDrawingBrush.color = "black";
+  canvas.setBackgroundColor("lightgray", canvas.renderAll.bind(canvas));
 };
