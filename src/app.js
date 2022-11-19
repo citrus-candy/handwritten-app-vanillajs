e89a8fdd09a09221d5c35d941b37e7fff047299e
citrus-candy <kyuuri11013@gmail.com>
2022-11-20 02:18:59 +0900

    add: デザインの実装

--- src/app.js
+++ src/app.js
@@ -6,6 +6,6 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 };
